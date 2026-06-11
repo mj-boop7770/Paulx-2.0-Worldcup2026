a11fa0ceb122eb0c56d6e9c516029e2568e7722d
mj-boop7770 <mrloverflv@gmail.com>
2026-06-11 19:37:56 +0200

    Update chance.js

--- js/chance.js
+++ js/chance.js
@@ -1,1 +1,14 @@
+// js/chance.js - Le cerveau de MUJOS
 
+function castVote(matchId, selectedTeam, home, away) {
+    // 1. Sauvegarde le vote dans le téléphone
+    localStorage.setItem('vote_' + matchId, selectedTeam);
+    
+    // 2. MUJOS fait sa prédiction (50/50 pour le moment)
+    const prediction = Math.random() < 0.5 ? home : away;
+    
+    // 3. Message de confirmation simple
+    alert("Vote validé pour " + selectedTeam + "!\n\n🐙 MUJOS prédit : " + prediction);
+    
+    console.log("Vote enregistré pour le match " + matchId + " : " + selectedTeam);
+}
